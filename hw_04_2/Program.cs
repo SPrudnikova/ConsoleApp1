0880389087961affac1e10fcd9bc1d731a69f052
SPrudnikova <sprudnikava@exadel.com>
2019-03-20 22:02:49 +0100

    added hw_04_2

---
+++ hw_04_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace hw_04_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKey direction = Console.ReadKey().Key;
+                Console.WriteLine();
+
+                switch (direction)
+                {
+                    case ConsoleKey.W:
+                        Console.WriteLine("Top");
+                        break;
+                    case ConsoleKey.S:
+                        Console.WriteLine("Down");
+                        break;
+                    case ConsoleKey.A:
+                        Console.WriteLine("Left");
+                        break;
+                    case ConsoleKey.D:
+                        Console.WriteLine("Right");
+                        break;
+                    default:
+                        Console.WriteLine("Direction is not defined");
+                        break;
+                }
+            }
+        }
+    }
+}
