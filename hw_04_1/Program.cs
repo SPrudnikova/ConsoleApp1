e166d990ecbd315513e0eff9fa42480250944f4a
SPrudnikova <sprudnikava@exadel.com>
2019-03-20 21:31:19 +0100

    added hw_04_1

---
+++ hw_04_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace hw_04_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 90; i >= 65; i--)
+            {
+                Console.Write((char)i);
+            }
+            Console.Read();
+        }
+    }
+}
