c7bfe304356d67ea08c56f195a5293f65b808585
SPrudnikova <sprudnikava@exadel.com>
2019-03-21 17:34:21 +0100

    added empty array creation

---
+++ practice_21_03_2019/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_21_03_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] emptyArr = CreateEmptyIntArray();
+        }
+
+        public static int[] CreateEmptyIntArray()
+        {
+            return new int[0];
+        }
+    }
+}
