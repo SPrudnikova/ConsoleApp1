38bd865c8b24b3a59b5fe778743fd967e4b7129b
SPrudnikova <sprudnikava@exadel.com>
2019-03-21 17:42:09 +0100

    added task2

--- practice_21_03_2019/Program.cs
+++ practice_21_03_2019/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace practice_21_03_2019
 {
@@ -10,12 +6,44 @@ namespace practice_21_03_2019
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Task 1");
             int[] emptyArr = CreateEmptyIntArray();
+            Console.WriteLine($"Empty array length: {emptyArr.Length}");
+            Console.WriteLine();
+
+            Console.WriteLine("Task 2");
+            CreateMixedArray();
+            Console.WriteLine();
+
+            Console.Read();
         }
 
         public static int[] CreateEmptyIntArray()
         {
             return new int[0];
+        }
+
+        public static void CreateMixedArray()
+        {
+            dynamic[] mixedArray= new dynamic[3];
+            mixedArray[0] = 32;
+            mixedArray[1] = 'A';
+            mixedArray[2] = "Hello";
+
+            for (int i = 0; i < mixedArray.Length; i++)
+            {
+                Console.Write($"{mixedArray[i]} ");
+            }
+
+            Console.WriteLine();
+
+            mixedArray[0] = mixedArray[0] + 10;
+            mixedArray[2] = mixedArray[2] + ", guys!";
+
+            for (int i = 0; i < mixedArray.Length; i++)
+            {
+                Console.Write($"{mixedArray[i]} ");
+            }
         }
     }
 }
