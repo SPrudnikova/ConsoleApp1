33b13133bcc7e9f71b45609dd5bba5df7ba0ee2d
SPrudnikova <sprudnikava@exadel.com>
2019-03-21 10:33:00 +0100

    added hw_05

---
+++ hw_05/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace hw_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 1:");
+            Task1();
+            Console.WriteLine();
+
+            Console.WriteLine("Task 2:");
+            Task2();
+            Console.WriteLine();
+
+            Console.WriteLine("Task 3:");
+            Task3();
+            Console.WriteLine();
+
+            Console.Read();
+        }
+
+        public static void Task1()
+        {
+            int arrSize = 5;
+
+            int[] randomArr = FillArrayRandom(new int[arrSize], 0, arrSize);
+
+            Console.WriteLine($"Enter {arrSize} integer to fill an array");
+            int[] userArr = FillArrayByUser(new int[arrSize], 0, arrSize);
+
+            int[] resultArr = new int[arrSize];
+            for (int i = 0; i < arrSize; i++)
+            {
+                resultArr[i] = userArr[i] + randomArr[i];
+            }
+
+            Console.WriteLine("Random array values:");
+            PrintArray(randomArr);
+            Console.WriteLine();
+
+            Console.WriteLine("User array values:");
+            PrintArray(userArr);
+            Console.WriteLine();
+
+            Console.WriteLine("Result array values:");
+            PrintArray(resultArr);
+            Console.WriteLine();
+        }
+
+        public static void Task2()
+        {
+            int arrSize = 10;
+            int arrFillSize = 9;
+
+            Console.WriteLine($"Enter {arrFillSize} integer to fill array");
+            int[] userArr = FillArrayByUser(new int[arrSize], 0, arrFillSize);
+
+            Console.WriteLine("Initial array:");
+            PrintArray(userArr);
+            Console.WriteLine();
+
+            Console.WriteLine($"Enter insert index from 0 to {arrSize - 1}:");
+            int insertIndex = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter integer to insert");
+            int insertValue = Convert.ToInt32(Console.ReadLine());
+
+            Insert(userArr, insertIndex, insertValue);
+
+            Console.WriteLine("Result array:");
+            PrintArray(userArr);
+        }
+
+        public static void Task3()
+        {
+            int arrSize = 11;
+            int[] randomArr = FillArrayRandom(new int[11], 0, arrSize);
+
+            Console.WriteLine("Initial array:");
+            PrintArray(randomArr);
+            Console.WriteLine();
+
+            Reverse(randomArr);
+
+            Console.WriteLine("Result array:");
+            PrintArray(randomArr);
+
+        }
+
+        public static void PrintArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+
+        public static int[] Insert(int[] arr, int index, int value)
+        {
+            for (int i = arr.Length - 1; i >= index; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[index] = value;
+
+            return arr;
+        }
+
+        public static int[] FillArrayByUser(int[] arr, int from, int to)
+        {
+            for (int i = from; i < to; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            return arr;
+        }
+
+        public static int[] FillArrayRandom(int[] arr, int from, int to)
+        {
+            Random randomGenerator = new Random();
+            for (int i = from; i < to; i++)
+            {
+                arr[i] = randomGenerator.Next(0, 100);
+            }
+
+            return arr;
+        }
+
+        public static T[] Reverse<T>(T[] arr)
+        {
+            int arrHalf = arr.Length / 2;
+            for (int i = 0; i < arrHalf; i++)
+            {
+                int reverseIndex = arr.Length - 1 - i;
+                var reverseValue = arr[reverseIndex];
+
+                arr[reverseIndex] = arr[i];
+                arr[i] = reverseValue;
+            }
+
+            return arr;
+        }
+    }
+}
