13b6f72792616f504e7d502b5e8c0e22c272518e
SPrudnikova <sprudnikava@exadel.com>
2019-04-23 19:35:29 +0200

    added ellipsise extention

---
+++ StringEllipsise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringEllipsise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter string:");
+            string userString = Console.ReadLine();
+            Console.WriteLine(userString.Ellipsise());
+
+            Console.Read();
+        }
+    }
+
+    static class StringExtention {
+        public static string Ellipsise (this string str)
+        {
+            if (str.Length > 5)
+            {
+                return $"{str.Substring(0, 5)}...";
+            }
+            return str;
+        }
+    }
+}
