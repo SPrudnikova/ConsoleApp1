9c21af9bb3f2c35a0038512d6b9ccca4d9558236
SPrudnikova <sprudnikava@exadel.com>
2019-04-24 22:14:10 +0200

    hm_13 task1

---
+++ hw_13/Stack.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace hw_13
+{
+    class Stack
+    {
+        private const int capacity = 10;
+        private object[] stack { get; set; }
+        private int stackSize { get; set; }
+
+        public Stack()
+        {
+            stackSize = 0;
+            stack = new object[capacity];
+        }
+
+        public void Push(object item)
+        {
+            if (capacity == stackSize)
+            {
+                throw new StackException("Stack overflow");
+            }
+
+            stack[stackSize] = item;
+            stackSize += 1;
+        }
+
+        public object Pop()
+        {
+            if (stackSize == 0) {
+                throw new StackException("Stack is empty");
+            }
+
+            object item = stack[stackSize - 1];
+            stack[stackSize - 1] = null;
+            stackSize -= 1;
+
+            return item;
+        }
+
+        public object Peek()
+        {
+            return stack[stackSize - 1];
+        }
+    }
+}
---
+++ hw_13/StackException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hw_13
+{
+    class StackException : Exception
+    {
+        public StackException()
+        {
+        }
+
+        public StackException(string message)
+            : base(message)
+        {
+        }
+
+        public StackException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
